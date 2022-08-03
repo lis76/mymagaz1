b0381b1d7c55876ddec5f776f508f3f9f430bedb
lis76 <rsavoskin@yandex.ru>
2022-08-03 09:44:54 +0300

    first commit

---
+++ app/models/question.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Question < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :body, presence: true, length: { minimum: 3 }
+
+  def formatted_created_at
+    created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
+end
